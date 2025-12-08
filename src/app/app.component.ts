2ee512f47a4f0919678dfeee7d523d43ef349838
mikeychowy <chowmichaelworkmail@gmail.com>
2025-12-08 23:22:41 +0700

    feat: start repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+export interface Data {
+  _id: string;
+  name: string;
+  age: number;
+  email: string;
+  phone: string;
+  address: string;
+  balance: string;
+  isActive: boolean;
+}
+
+/** Constants used to fill up our data base. */
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'age', 'email', 'phone', 'address', 'balance', 'isActive'];
+  dataSource: MatTableDataSource<Data>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | null;
+  @ViewChild(MatSort) sort: MatSort | null;
+
+  constructor() {
+    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+    this.sort = null;
+    this.paginator = null;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new Data. */
+function createNewUser(id: number): Data {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    _id: id.toString(),
+    name: name,
+    age: Math.round(Math.random() * 100),
+    email: 'asd@asd.com',
+    address: 'anywhere',
+    balance: '$3,722.51',
+    phone: '+1 (908) 583-2056',
+    isActive: true,
+  };
 }
