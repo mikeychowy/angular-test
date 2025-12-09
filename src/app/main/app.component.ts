0509c145ed315e9ba8fe1edbc0bf2af627336a1a
mikeychowy <chowmichaelworkmail@gmail.com>
2025-12-10 01:28:44 +0700

    feat: add new paginator logic

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, inject, signal, ViewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
@@ -8,6 +8,7 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { map, startWith, tap } from 'rxjs/operators';
+import { ButtonPaginatorComponent } from '../paginator/button-paginator.component';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,7 @@ import { map, startWith, tap } from 'rxjs/operators';
     MatSortModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
+    ButtonPaginatorComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -28,16 +30,19 @@ export class AppComponent implements AfterViewInit {
   dataSource: MatTableDataSource<Data>;
 
   data: Data[] = [];
+  dataLength = signal(this.data.length);
   isLoadingResults = true;
 
   @ViewChild(MatPaginator) paginator: MatPaginator | null;
   @ViewChild(MatSort) sort: MatSort | null;
+  @ViewChild(ButtonPaginatorComponent) customPaginator: ButtonPaginatorComponent | null;
 
   constructor() {
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(this.data);
     this.sort = null;
     this.paginator = null;
+    this.customPaginator = null;
   }
 
   ngAfterViewInit() {
@@ -52,6 +57,8 @@ export class AppComponent implements AfterViewInit {
           if (data === null) {
             return [];
           }
+
+          this.dataLength.set(data.length);
           return data;
         })
       )
@@ -65,6 +72,10 @@ export class AppComponent implements AfterViewInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.customPaginator) {
+      this.dataLength.set(this.dataSource._filterData.length);
+    }
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
--- src/app/paginator/button-paginator.component.ts
+++ src/app/paginator/button-paginator.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnChanges, SimpleChanges, input } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, computed, input, numberAttribute, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -7,22 +7,39 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './button-paginator.component.html',
   styleUrl: './button-paginator.component.scss',
 })
-export class ButtonPaginatorComponent implements AfterViewInit, OnChanges {
-  appCustomLength = input.required<number>();
+export class ButtonPaginatorComponent implements OnInit, OnChanges {
+  pageSize = input.required({ transform: numberAttribute });
+  dataLength = input.required({ transform: numberAttribute });
 
-  ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+  currentPage = signal(0);
+
+  currentPageNumber = computed(() => (this.dataLength() > 0 ? this.currentPage() : 0));
+
+  pageLength = computed(() => calculatePagesLength(this.dataLength(), this.pageSize()));
+  hasNextPage = computed(() => this.pageLength() > this.currentPage());
+  hasPreviousPage = computed(() => this.currentPage() >= this.pageLength());
+
+  ngOnInit(): void {
+    this.currentPage.set(1);
   }
   /**
    * react on parent component changing the appCustomLength - rerender bubbles
    */
   ngOnChanges(changes: SimpleChanges): void {
-    if (!changes?.['appCustomLength']?.firstChange) {
+    if (!changes?.['dataLength']?.firstChange) {
       // remove buttons before creating new ones
-      this.removeButtons();
-      // switch back to page 0
-      this.switchPage(0);
-      this.renderButtons();
+      // this.removeButtons();
+      // // switch back to page 0
+      // this.switchPage(0);
+      // this.renderButtons();
     }
   }
+
+  reset(): void {
+    this.currentPage.set(0);
+  }
+}
+
+function calculatePagesLength(dataLength: number, pageSize: number): number {
+  return Math.ceil(dataLength / pageSize);
 }
